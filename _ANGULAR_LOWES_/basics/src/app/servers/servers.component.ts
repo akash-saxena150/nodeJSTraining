694178c4ba6888f81942608343f2d5544b92c395
akash@getsetgo.fitness <akash@getestgo.fitness>
2018-10-23 18:00:05 +0530

    Added more angular files

--- _ANGULAR_LOWES_/basics/src/app/servers/servers.component.ts
+++ _ANGULAR_LOWES_/basics/src/app/servers/servers.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-
+  serverCreatedStatus = "Waiting to create a server"
   constructor() { }
 
   ngOnInit() {
+  }
+  createServer(e){
+    console.log(e);
+    this.serverCreatedStatus = "Server created";
   }
 
 }
