4ab44276b2cb117c5bfd19a0f5ad336fa3069bcf
akash@getsetgo.fitness <akash@getestgo.fitness>
2018-10-25 12:54:38 +0530

    checked in the project files

--- _ANGULAR_LOWES_/cmp-data-binding/src/app/server-element/server-element.component.ts
+++ _ANGULAR_LOWES_/cmp-data-binding/src/app/server-element/server-element.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: {name: string, type: string, content:string}
+  @Input('srvElement') element: {name: string, type: string, content:string}
   constructor() { 
     console.log(typeof(this.element));
   }
