a090f6a83e6b8306f22b52027b3a54858fb57cd1
akash@getsetgo.fitness <akash@getestgo.fitness>
2018-10-25 14:34:32 +0530

    checked in the project files

--- _ANGULAR_LOWES_/cmp-data-binding/src/app/cockpit/cockpit.component.ts
+++ _ANGULAR_LOWES_/cmp-data-binding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,6 +8,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter <{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter <{serverName: string, serverContent: string}>();
+  @ViewChild('serverName') serverName:ElementRef
   newServerName = '';
   newServerContent = '';
   constructor() { }
@@ -20,7 +21,8 @@ export class CockpitComponent implements OnInit {
     //   name: this.newServerName,
     //   content: this.newServerContent
     // });
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent})
+    console.log(this.serverName.nativeElement.value);
+    this.serverCreated.emit({serverName: this.serverName.nativeElement.value, serverContent: this.newServerContent})
   }
 
   onAddBlueprint() {
