ad45fe9f92fe28d8f2585ca257ab561ed8e4d5ed
pawarsh <shashank.pawar@tecnotree.com>
2020-02-20 11:14:11 +0530

    checked in the reactive form

--- GSLabs_130220/forms-td-start/src/app/app.component.ts
+++ GSLabs_130220/forms-td-start/src/app/app.component.ts
@@ -7,11 +7,40 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signupform: NgForm
+  @ViewChild('f') signupform: NgForm;
+  a:number = 10;
+  answer='';
+  user={
+    username: '',
+    email: '',
+    question:'',
+    answer: '' 
+  };
+  submitted = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupform.setValue({
+    //     userData: {
+    //       username: suggestedName,
+    //       email: '',
+    //     },
+    //       secret: 'pet',
+    //       questionAnswer: ''      
+    //   })
+    this.signupform.form.patchValue({
+      userData:{
+        username: suggestedName
+      }
+    })
+    
   }
   formSubmit(f){
-    console.log(f);
+    console.log(this.signupform);
+    this.submitted = true;
+    let userData = this.signupform.value;
+    this.user.username = userData.userData.username;
+    this.user.email = userData.userData.email;
+    this.user.question = userData.secret;
+    this.user.answer = userData.questionAnswer
   }
 }
